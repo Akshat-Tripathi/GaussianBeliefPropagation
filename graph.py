a8810a329f85f39bc6f91dcec98bf856f7b4d43e
Akshat Tripathi <akshat.tripathi6568@gmail.com>
2022-11-10 10:55:36 +0000

    Fixed bugs

--- graph.py
+++ graph.py
@@ -16,25 +16,28 @@ class VariableNode:
         for (eta, Lambda) in self.messages.values():
             self.eta += eta
             self.Lambda += Lambda
+
+    def get_dist(self):
+        # return self.Lambda * self.eta, 1/self.Lambda
+        return self.eta, self.Lambda
     
-    def send_message(self, factor: FactorNode):
+    def calculate_message(self, factor):
         m_eta = 0
         m_Lambda = 0
         for (link, (eta, Lambda)) in self.messages.items():
             if link != factor:
                 m_eta += eta
                 m_Lambda += Lambda
-        
-        outlink.messages[self] = (m_eta, m_Lambda)
+        return m_eta, m_Lambda
 
     def send_messages(self):
         for link in self.links:
-            self.send_message(link)
+            m_eta, m_Lambda = self.calculate_message(link)
+            link.messages[self] = (m_eta, m_Lambda)
 
 class FactorNode:
-    def __init__(self, variables: List[VariableNode], eta: np.ndarray, Lambda: np.ndarray):
-        self.eta = eta
-        self.Lambda = Lambda
+    def __init__(self, variables: List[VariableNode], mean: np.ndarray, cov: np.ndarray):
+        self.update(mean, cov)
 
         for var in variables:
             var.links.append(self)
@@ -42,34 +45,46 @@ class FactorNode:
 
         self.messages = {}
 
-    def send_message(self, var: VariableNode):
+    def set_eta_lambda(self, eta, Lambda):
+        self.eta = eta
+        self.Lambda = Lambda
+
+    def calculate_message(self, var: VariableNode):
         eta_prime = self.eta.copy()
         Lambda_prime = self.Lambda.copy()
         
         for (i, link) in enumerate(self.links):
             if link != var:
-                (_, (eta, Lambda)) = self.messages[link]
+                (eta, Lambda) = self.messages.setdefault(link, (0, 0))
                 eta_prime[i] += eta
                 Lambda_prime[i, i] += Lambda
-        
+
         # Marginalise to get a scalar message
         var_idx = self.links.index(var)
         eta_prime[0], eta_prime[var_idx] = eta_prime[var_idx], eta_prime[0]
-        Lambda_prime[0, :], Lambda_prime[var_idx, :] = Lambda_prime[var_idx, :], Lambda_prime[0, :]
-        Lambda_prime[:, 0], Lambda_prime[:, var_idx] = Lambda_prime[:, var_idx], Lambda_prime[:, 0]
+        Lambda_prime[[0, var_idx]] = Lambda_prime[[var_idx, 0]]
+        Lambda_prime[:, [0, var_idx]] = Lambda_prime[:, [var_idx, 0]]
 
         eta_a = eta_prime[0]
         eta_b = eta_prime[1:]
-        Lambda_a_a = Lambda_prime[var_idx, var_idx]
-        Lambda_a_b = Lambda_prime[var_idx, 1:]
-        Lambda_b_a = Lambda_prime[1:, var_idx]
+        Lambda_a_a = Lambda_prime[0, 0]
+        Lambda_a_b = Lambda_prime[0, 1:]
+        Lambda_b_a = Lambda_prime[1:, 0]
         Lambda_b_b = Lambda_prime[1:, 1:]
 
-        partial_dot = np.dot(Lambda_a_b, Lambda_b_b.I)
+        partial_dot = np.dot(Lambda_a_b, np.linalg.inv(Lambda_b_b))
         m_eta = eta_a - np.dot(partial_dot, eta_b)
         m_Lambda = Lambda_a_a - np.dot(partial_dot, Lambda_b_a)
 
+        # print(eta_a, eta_b, Lambda_a_a, Lambda_a_b, Lambda_b_b, Lambda_b_a, m_eta, m_Lambda)
+
+        return m_eta, m_Lambda
+
     def send_messages(self):
         for link in self.links:
-            self.send_message(link)
+            eta, Lambda = self.calculate_message(link)
+            link.messages[self] = (eta, Lambda)
 
+    def update(self, mean: np.ndarray, cov: np.ndarray):
+        self.Lambda = np.linalg.inv(cov)
+        self.eta = np.dot(self.Lambda, mean)
--- main.py
+++ main.py
@@ -1,5 +1,25 @@
-from graph import FactorGraph
-
+from graph import FactorNode, VariableNode
 import numpy as np
 
-print(np.zeros(3))
+v1 = VariableNode()
+v2 = VariableNode()
+
+f1 = FactorNode([v1], np.array([5]), np.array([[1]]))
+f2 = FactorNode([v1, v2], np.array([1, 2]), np.array([[5, 4], [4, 6]]))
+# f2.set_eta_lambda(np.array([1, 2]), np.array([[5, -4], [-4, 6]]))
+
+f1.send_messages()
+v1.send_messages()
+f2.send_messages()
+v2.send_messages()
+f2.send_messages()
+v1.send_messages()
+
+# print(v2.messages)
+
+v1.belief_update()
+v2.belief_update()
+
+# print(v1.get_dist())
+print(v1.eta, v1.Lambda)
+print(v2.eta, v2.Lambda)
